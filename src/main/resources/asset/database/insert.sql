94ebc66b87f9f8bed7bbb8b9ef2efba8017a1078
anhthu100903 <anhthu115599@gmail.com>
2024-10-09 23:21:07 +0700

    fix insert.sql and add img

--- src/main/resources/asset/database/insert.sql
+++ src/main/resources/asset/database/insert.sql
@@ -1,55 +1,56 @@
 -- Chèn dữ liệu vào bảng Author
 INSERT INTO `Author` (`name`, `year`, `isActive`) VALUES
-('Nguyễn Nhật Ánh', 1955, 1),
-('Xuân Diệu', 1916, 0),
-('Nam Cao', 1915, 0),
-('Thach Lam', 1910, 1),
-('Nguyễn Tuân', 1910, 0),
-('Ngô Tất Tố', 1893, 0),
-('Vũ Trọng Phụng', 1912, 1),
-('Thanh Tịnh', 1911, 0),
-('Nguyên Hồng', 1918, 0),
-('Nguyên Ngọc', 1932, 0);
+('Pamela Hutchinson', 1953, 1),
+('Dale Carnegie', 1888, 1),
+('Jo Condrill, Bennie Bough', null, 1),
+('Darrell Mullis & Judith Orloff', 1910, 1),
+('Trác Nhã', 1929, 1),
+('John C. Maxwell', 1947, 1),
+('Bộ Thông tin và Truyền thông', null, 1),
+('Kevin Mitnick', 1963, 0),
+('Clifford Stoll', 1950, 0),
+('Tony Buổi Sáng', 1978, 0),
+('Dương Ngọc Duy', 1990, 0);
 
 -- Chèn dữ liệu vào bảng Category
 INSERT INTO `Category` (`name`, `isActive`) VALUES
-('Văn học cổ điển', 1),
-('Văn học hiện đại', 1),
-('Khoa học tự nhiên', 1),
-('Khoa học xã hội', 1),
-('Kinh tế học', 1),
-('Quản trị kinh doanh', 1),
-('Kỹ năng sống', 1),
-('Khoa học công nghệ', 1),
-('Văn học thiếu nhi', 1),
-('Phát triển bản thân', 1);
+('Điện ảnh, kỹ thuật làm phim', 1),
+('Tâm lý - Kỹ năng sống', 1),
+('Kỹ năng giao tiếp', 1),
+('Khởi Nghiệp Kinh Doanh Thực Chiến', 1),
+('Phát triển bản thân', 1),
+('Tài liệu chuyên khảo về công nghệ thông tin và truyền thông', 1),
+('Công nghệ thông tin, An ninh mạng', 1),
+('Văn học thiếu nhi', 0),
+('Phát triển bản thân', 0);
 
 
 -- Chèn dữ liệu vào bảng Publisher
 INSERT INTO `Publisher` (`name`, `isActive`) VALUES
 ('Nhà xuất bản Kim Đồng', 1),
+('Nhà xuất bản Tổng hợp TPHCM', 1),
+('Nhà xuất bản Công Thương', 1),
+('Nhà xuất bản Thế Giới', 1),
+('Nhà xuất bản Văn học', 1),
+('Nhà xuất bản Thông tin và Truyền thông', 1),
+('Nhà xuất bản Hồng Đức', 1),
 ('Nhà xuất bản Trẻ', 1),
-('Nhà xuất bản Giáo dục', 1),
-('Nhà xuất bản Văn Học', 1),
-('Nhà xuất bản Lao Động', 1),
-('Nhà xuất bản Tổng Hợp', 1),
-('Nhà xuất bản Văn Nghệ', 1),
-('Nhà xuất bản Công An Nhân Dân', 1),
-('Nhà xuất bản Thanh Niên', 1),
-('Nhà xuất bản Phụ Nữ', 1);
+('Nhà xuất bản Thanh Niên', 0),
+('Nhà xuất bản Phụ Nữ', 0);
 
 -- Chèn dữ liệu vào bảng Book
 INSERT INTO `Book` (`name`, `isActive`) VALUES
-('Cho Tôi Xin Một Vé Đi Tuổi Thơ', 1),
-('Vợ Nhặt', 1),
-('Sống Mòn', 1),
-('Tắt Đèn', 1),
-('Lão Hạc', 1),
-('Đất Rừng Phương Nam', 1),
-('Dế Mèn Phiêu Lưu Ký', 1),
-('Bước Đường Cùng', 1),
-('Truyện Kiều', 1),
-('Chí Phèo', 1);
+('30 Giây Điện Ảnh', 1),
+('Đắc Nhân Tâm', 1),
+('Giao Tiếp Với Bất Kỳ Ai', 1),
+('Kế Toán Vỉa Hè', 1),
+('Khéo Ăn Nói Sẽ Có Được Thiên Hạ', 1),
+('Nâng Tầm Ảnh Hưởng', 1),
+('Sách Trắng Công Nghệ Thông Tin Và Truyền Thông', 1),
+('Nghệ Thuật Ẩn Mình', 1),
+('Gián Điệp Mạng', 1),
+('Cafe Cùng Tony', 1),
+('Đời Đổi Thay Khi Chúng Ta Thay Đổi', 1);
 
 -- Chèn dữ liệu vào bảng BookAuthor
 INSERT INTO `BookAuthor` (`authorID`, `bookID`) VALUES
@@ -62,94 +63,110 @@ INSERT INTO `BookAuthor` (`authorID`, `bookID`) VALUES
 (7, 7),
 (8, 8),
 (9, 9),
-(10, 10);
+(10, 10),
+(11, 11);
 
 -- Chèn dữ liệu vào bảng BookCategory
 INSERT INTO `BookCategory` (`categoryID`, `bookID`) VALUES
 (1, 1),
-(1, 2),
-(1, 3),
-(1, 4),
-(1, 5),
-(1, 6),
-(1, 7),
-(1, 8),
-(1, 9),
-(1, 10);
+(2, 2),
+(3, 3),
+(4, 4),
+(3, 5),
+(5, 6),
+(6, 7),
+(7, 8),
+(7, 9),
+(5, 10),
+(5, 11);
 
 -- Chèn dữ liệu vào bảng VersionOfBook
 INSERT INTO `VersionOfBook` (`ISBN`, `bookID`, `img`, `publisherID`, `edition`, `location`, `price`, `quantity`, `available`) VALUES
-('978-604-2-10764-5', 1, '/asset/img/30giaykhoahoc.webp', 2, '1st', 'Hà Nội', 100000, 50, 50),
-('978-604-2-23577-2', 2, '/asset/img/baovenguoitocao.jpg', 1, '2nd', 'TP.HCM', 120000, 40, 35),
-('978-604-3-40077-3', 3, '/asset/img/dacnhantam.jpg', 3, '3rd', 'Đà Nẵng', 150000, 30, 25),
-('978-604-3-40077-4', 4, '/asset/img/giaotiepbatkyai.png', 4, '1st', 'Huế', 110000, 40, 30),
-('978-604-3-40077-5', 5, '/img/ketoanviahe.png', 5, '1st', 'Đà Lạt', 130000, 35, 20),
-('978-604-3-40077-6', 6, '/img/kheoankheonoi.png', 6, '2nd', 'Hà Nội', 160000, 50, 45),
-('978-604-3-40077-7', 7, '/asset/img/khoahoc.webp', 7, '1st', 'TP.HCM', 175000, 25, 15),
-('978-604-3-40077-8', 8, 'src/main/java/asset/img/khoinghiep.jpg', 8, '2nd', 'Đà Nẵng', 200000, 20, 15),
-('978-604-3-40077-9', 9, 'src/main/java/asset/img/nangtamanhhuong.png', 9, '1st', 'Huế', 195000, 45, 35),
-('978-604-3-40078-0', 10, 'src/main/java/asset/img/sachtrangcntt.png', 10, '3rd', 'Đà Lạt', 220000, 30, 25);
+('978-604-2-28251-2', 1, './src/main/resources/asset/img/30giaydienanh.webp', 1, '1st', 'Hà Nội', 140000, 50, 50),
+('978-604-3-92743-6', 2, './src/main/resources/asset/img/dacnhantam.jpg', 2, '3rd', 'Đà Nẵng', 80000, 30, 25),
+('978-604-4-02274-1', 3, './src/main/resources/asset/img/giaotiepbatkyai.png', 3, '1st', 'Huế', 50000, 40, 30),
+('978-604-7-78761-6', 4, './src/main/resources/asset/img/ketoanviahe.png', 4, '1st', 'Đà Lạt', 130000, 35, 20),
+('978-604-3-62922-4', 5, './src/main/resources/asset/img/kheoankheonoi.png', 5, '2nd', 'Hà Nội', 138000, 50, 45),
+('978-604-553-860-4', 6, './src/main/resources/asset/img/nangtamanhhuong.png', 3, '1st', 'TP.HCM', 88000, 25, 15),
+('978-604-952-579-5', 7, './src/main/resources/asset/img/sachtrangcntt.png', 6, '3rd', 'Đà Lạt', 220000, 30, 25),
+('978-604-87-0693-1', 8, './src/main/resources/asset/img/nghethuatnminh.jpg', 7, '2nd', 'Đà Nẵng', 130000, 20, 15),
+('978-604-1-08637-4', 9, './src/main/resources/asset/img/giandiepmang.jpg', 8, '1st', 'Huế', 140000, 45, 35),
+('978-604-0-15014-8', 10, './src/main/resources/asset/img/cafecungtony.jpg', 8, '1st', 'Huế', 80000, 40, 32),
+('978-604-56-0506-3', 11, './src/main/resources/asset/img/doidoithaykhichungtathaydoi.jpg', 2, '1st', 'Huế', 95000, 20, 16);
 
 -- Chèn dữ liệu vào bảng Person
 INSERT INTO `Person` (`id`, `name`, `tel`, `address`, `schoolYear`, `isActive`) VALUES
-('A123456789', 'Nguyễn Văn A', '0909123456', 'Hà Nội', '2023-2024', 1),
-('B987654321', 'Trần Thị B', '0912345678', 'TP.HCM', '2022-2023', 1),
-('C567890123', 'Lê Văn C', '0923456789', 'Đà Nẵng', '2024-2025', 1),
-('D123456780', 'Nguyễn Thị D', '0908765432', 'Huế', '2021-2022', 1),
-('E234567891', 'Lê Thị E', '0919876543', 'Đà Lạt', '2020-2021', 1),
-('F345678912', 'Phan Văn F', '0921987654', 'Quảng Ninh', '2019-2020', 1),
-('G456789123', 'Trần Văn G', '0932198765', 'Nha Trang', '2022-2023', 1),
-('H567891234', 'Phạm Thị H', '0943219876', 'Hải Phòng', '2023-2024', 1),
-('I678912345', 'Ngô Văn I', '0954321987', 'Cần Thơ', '2024-2025', 1),
-('J789123456', 'Lê Văn J', '0965432198', 'Biên Hòa', '2023-2024', 1);
+('SV00000001', 'Nguyễn Văn A', '0909123456', 'Hà Nội', '2023-2024', 1),
+('SV00000002', 'Trần Thị B', '0912345678', 'TP.HCM', '2022-2023', 1),
+('SV00000003', 'Lê Văn C', '0923456789', 'Đà Nẵng', '2024-2025', 1),
+('SV00000004', 'Nguyễn Thị D', '0908765432', 'Huế', '2021-2022', 1),
+('SV00000005', 'Lê Thị E', '0919876543', 'Đà Lạt', '2020-2021', 1),
+('SV00000006', 'Phan Văn F', '0921987654', 'Quảng Ninh', '2019-2020', 1),
+('SV00000007', 'Trần Văn G', '0932198765', 'Nha Trang', '2022-2023', 1),
+('GV00000001', 'Phạm Thị H', '0943219876', 'Hải Phòng', null, 1),
+('GV00000002', 'Ngô Văn I', '0954321987', 'Cần Thơ', null, 1),
+('GV00000003', 'Lê Văn J', '0965432198', 'Biên Hòa', null, 1),
+('TT00000001', 'Phan Thị Anh Thư', '0901236548', 'Tiền Giang', null, 1),
+('AD00000001', 'Trần Bảo Trân', '0901244444', 'TP HCM', null, 1),
+('TK00000001', 'Trần Đức An', '0901228399', 'TP HCM', null, 1),
+('QL00000001', 'Trần Thị Thủy', '0901446548', 'Quảng Trị', null, 1),
+('QL00000002', 'Nguyễn Thanh Tiến', '0922236548', 'Bình Dương', null, 1);
 
 -- Chèn dữ liệu vào bảng Role
 INSERT INTO `Role` (`id`, `name`) VALUES
 ('AD', 'Admin'),
 ('TT', 'Thủ thư'),
 ('TK', 'Thủ kho'),
-('QL', 'Quản vý'),
+('QL', 'Quản lý'),
 ('GV', 'Giảng viên'),
-('SV', 'Sinh viên')
+('SV', 'Sinh viên');
 
 -- Chèn dữ liệu vào bảng Account
 INSERT INTO `Account` (`id`, `password`, `positionID`) VALUES
-('A123456789', 'password123', '01'),
-('B987654321', 'password456', '02'),
-('C567890123', 'password789', '03'),
-('D123456780', 'password234', '02'),
-('E234567891', 'password345', '01'),
-('F345678912', 'password456', '03'),
-('G456789123', 'password567', '01'),
-('H567891234', 'password678', '02'),
-('I678912345', 'password789', '02'),
-('J789123456', 'password890', '03');
+('SV00000001', '123456', 'SV'),
+('SV00000002', '123456', 'SV'),
+('SV00000003', '123456', 'SV'),
+('SV00000004', '123456', 'SV'),
+('SV00000005', '123456', 'SV'),
+('SV00000006', '123456', 'SV'),
+('SV00000007', '123456', 'SV'),
+('GV00000001', '111111', 'GV'),
+('GV00000002', '111111', 'GV'),
+('GV00000003', '111111', 'GV'),
+('TT00000001', '000000', 'TT'),
+('AD00000001', '222222', 'AD'),
+('TK00000001', '333333', 'TK'),
+('QL00000001', '444444', 'QL'),
+('QL00000002', '444444', 'QL');
 
 -- Chèn dữ liệu vào bảng Borrowing
-INSERT INTO `Borrowing` (`readerID`, `borrowStaffID`, `dueDate`) VALUES
-('C567890123', 'B987654321', '2024-10-15'),
-('A123456789', 'B987654321', '2024-10-20'),
-('D123456780', 'B987654321', '2024-11-05'),
-('E234567891', 'A123456789', '2024-11-10'),
-('F345678912', 'A123456789', '2024-11-15'),
-('G456789123', 'A123456789', '2024-11-20'),
-('H567891234', 'C567890123', '2024-12-01'),
-('I678912345', 'C567890123', '2024-12-05'),
-('J789123456', 'D123456780', '2024-12-10'),
-('A123456789', 'D123456780', '2024-12-15');
+INSERT INTO `Borrowing` (`readerID`, `borrowStaffID`, `returnStaffID`, `borrowDate`, `dueDate`, `returnDate`)
+VALUES 
+('SV00000001', 'TT00000001', 'TT00000001', '2024-10-01', '2024-10-15', NULL),
+('SV00000003', 'TT00000001', NULL, '2024-10-03', '2024-10-17', NULL),
+('SV00000006', 'TT00000001', 'TT00000001', '2024-10-05', '2024-10-19', '2024-10-18'),
+('SV00000007', 'TT00000001', NULL, '2024-10-06', '2024-10-20', NULL),
+('SV00000005', 'TT00000001', 'TT00000001', '2024-10-01', '2024-10-15', NULL),
+('GV00000001', 'TT00000001', NULL, '2024-10-03', '2024-10-17', NULL),
+('GV00000002', 'TT00000001', NULL, '2024-10-05', '2024-10-19', NULL),
+('GV00000003', 'TT00000001', NULL, '2024-10-06', '2024-10-20', NULL),
+('SV00000002', 'TT00000001', 'TT00000001', '2024-10-07', '2024-10-21', '2024-10-22');
 
 -- Chèn dữ liệu vào bảng BorrowDetail
-INSERT INTO `BorrowDetail` (`borrowID`, `ISBN`, `quantity`) VALUES
-(1, '978-604-2-10764-5', 1),
-(2, '978-604-2-23577-2', 1),
-(3, '978-604-3-40077-3', 2),
-(4, '978-604-3-40077-4', 1),
-(5, '978-604-3-40077-5', 2),
-(6, '978-604-3-40077-6', 3),
-(7, '978-604-3-40077-7', 1),
-(8, '978-604-3-40077-8', 2),
-(9, '978-604-3-40077-9', 3),
-(10, '978-604-3-40078-0', 1);
+INSERT INTO `BorrowDetail` (borrowID, ISBN, quantity, description)
+VALUES 
+(1, '978-604-2-28251-2', 1, 'Mượn phiên bản 1st tại Hà Nội'),
+(2, '978-604-3-92743-6', 2, 'Mượn phiên bản 3rd tại Đà Nẵng'),
+(3, '978-604-4-02274-1', 1, 'Mượn phiên bản 1st tại Huế'),
+(4, '978-604-7-78761-6', 1, 'Mượn phiên bản 1st tại Đà Lạt'),
+(5, '978-604-3-62922-4', 1, 'Mượn phiên bản 2nd tại Hà Nội'),
+(1, '978-604-553-860-4', 1, 'Mượn phiên bản 1st tại TP.HCM'),
+(3, '978-604-952-579-5', 2, 'Mượn phiên bản 3rd tại Đà Lạt'),
+(2, '978-604-87-0693-1', 1, 'Mượn phiên bản 2nd tại Đà Nẵng'),
+(5, '978-604-1-08637-4', 1, 'Mượn phiên bản 1st tại Huế'),
+(4, '978-604-0-15014-8', 2, 'Mượn phiên bản 1st tại Huế'),
+(1, '978-604-56-0506-3', 1, 'Mượn phiên bản 1st tại Huế');
+
 
 -- Chèn dữ liệu vào bảng Supplier
 INSERT INTO `Supplier` (`name`, `address`, `tel`) VALUES
@@ -162,30 +179,36 @@ INSERT INTO `Supplier` (`name`, `address`, `tel`) VALUES
 ('Công ty Sách G', 'Hải Phòng', '0921098765'),
 ('Công ty Sách H', 'Cần Thơ', '0910987654'),
 ('Công ty Sách I', 'Biên Hòa', '0909876543'),
-('Công ty Sách J', 'Đà Lạt', '0898765432');
+('Công ty Sách J', 'Đà Lạt', '0898765432'),
+('Công ty Sách T', 'Tiền Giang', '0898568990');
+
 
 -- Chèn dữ liệu vào bảng Importing
 INSERT INTO `Importing` (`supplierID`, `staffID`, `fee`) VALUES
-(1, 'B987654321', 500000),
-(2, 'B987654321', 300000),
-(3, 'A123456789', 700000),
-(4, 'A123456789', 450000),
-(5, 'C567890123', 600000),
-(6, 'C567890123', 350000),
-(7, 'D123456780', 800000),
-(8, 'D123456780', 400000),
-(9, 'E234567891', 750000),
-(10, 'E234567891', 550000);
+(1, 'SV00000002', 500000),
+(2, 'SV00000002', 300000),
+(3, 'SV00000001', 700000),
+(4, 'SV00000001', 450000),
+(5, 'SV00000003', 600000),
+(6, 'SV00000003', 350000),
+(7, 'SV00000004', 800000),
+(8, 'SV00000004', 400000),
+(9, 'SV00000005', 750000),
+(10, 'SV00000005', 550000),
+(11, 'SV00000001', 550000);
+
 
 -- Chèn dữ liệu vào bảng ImportDetail
--- INSERT INTO `ImportDetail` (`importID`, `ISBN`, `quantity`) VALUES
--- (1, '978-604-2-10764-5', 50),
--- (2, '978-604-3-40077-3', 30),
--- (3, '978-604-3-40077-4', 40),
--- (4, '978-604-3-40077-5', 60),
--- (5, '978-604-3-40077-6', 20),
--- (6, '978-604-3-40077-7', 70),z
--- (7, '978-604-3-40077-8', 80),
--- (8, '978-604-3-40077-9', 90),
--- (9, '978-604-3-40078-0', 25),
--- (10, '978-604-2-23577-2', 35);
+INSERT INTO `ImportDetail` (importID, ISBN, quantity)
+VALUES 
+(1, '978-604-2-28251-2', 50),
+(2, '978-604-3-92743-6', 30),
+(3, '978-604-4-02274-1', 40),
+(4, '978-604-7-78761-6', 35),
+(5, '978-604-3-62922-4', 50),
+(6, '978-604-553-860-4', 25),
+(7, '978-604-952-579-5', 30),
+(8, '978-604-87-0693-1', 20),
+(9, '978-604-1-08637-4', 45),
+(10, '978-604-0-15014-8', 40),
+(11, '978-604-56-0506-3', 20);
